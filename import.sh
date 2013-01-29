1caf30685d62a6a4537c170e2e733d5af26c03c0
Bruce Baron <baron.br@gmail.com>
2013-01-29 16:54:37 -0500

    impl import.sh

--- import.sh
+++ import.sh
@@ -3,14 +3,10 @@ for dir in ../8260_code/chapter02*-calendar
 do
   branch=$(basename $dir)
   echo "branch = $branch"
-#    git checkout -b $branch
-  ls $dir
-#    cp -r ../8260_code/$branch/* .
-#    git st
-#    git add .
-#    git st
-#    git ci -m "$branch"
-#    git checkout master
-#    rm -rf src/ target/
-#    git st
+  git checkout -b $branch
+  cp -rv $dir .
+  git add .
+  git ci -m "$branch"
+  git checkout master
+  rmdir src
 done
