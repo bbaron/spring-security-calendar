5e9c29d67d71e2decccd6e0b7618b06e647e8a57
Bruce Baron <baron.br@gmail.com>
2013-01-29 17:05:03 -0500

    import.sh

--- import.sh
+++ import.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-for dir in ../8260_code/chapter01*-calendar
+for dir in ../8260_code/chapter02*-calendar
 do
   branch=$(basename $dir)
   echo "branch = $branch"
   git checkout -b $branch
-  cp -rv $dir/* .
+  cp -r $dir/* .
   git add .
   git ci -m "$branch"
   git checkout master
